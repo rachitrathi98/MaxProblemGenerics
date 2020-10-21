61de8185ec3d9de0f2e6bef43866009e9b480f8a
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-21 21:04:08 +0530

    [Rachit] Add. Refactor Generic Method to find Max

--- MaxNumberGenerics/MaxNum.cs
+++ MaxNumberGenerics/MaxNum.cs
@@ -6,39 +6,7 @@ namespace MaxNumberGenerics
 {
     public class MaxNum
     {
-        public static int Max(int a, int b, int c)
-        {
-            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
-            {
-                return a;
-                    
-            }
-            else if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
-            {
-                return b;
-            }
-            else
-            {
-                return c;
-            }
-        }
-        public static double Max(double a, double b, double c)
-        {
-            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
-            {
-                return a;
-
-            }
-            else if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
-            {
-                return b;
-            }
-            else
-            {
-                return c;
-            }
-        }
-        public static string Max(string a, string b, string c)
+        public static T Max<T>(T a, T b, T c) where T : IComparable
         {
             if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
             {
