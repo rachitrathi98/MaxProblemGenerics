9945aa5acce979c42f19fb732c7696b1259bcfea
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-21 22:25:47 +0530

    [Rachit] Add. Passing multiple parameters

--- MaxNumberGenerics/MaxNum.cs
+++ MaxNumberGenerics/MaxNum.cs
@@ -6,18 +6,12 @@ namespace MaxNumberGenerics
 {
     public class MaxNum<T> where T : IComparable
     {
-        public static T maximum;
-
-        public static void Max(T[] arr)
+        public static  T Max(T[] arr)
         {
               Array.Sort(arr);
-            maximum= arr[arr.Length-1];
-            PrintMax(maximum);
+            return arr[arr.Length-1];
         }
-        public static void PrintMax(T max)
-        {
-            Console.WriteLine("Max Value for " + typeof(T) + " is: " + max);
-        }
+        
 
     }
 }
--- MaxNumberGenerics/Program.cs
+++ MaxNumberGenerics/Program.cs
@@ -6,18 +6,20 @@ namespace MaxNumberGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Generics Code!");
             Console.WriteLine();
             int[] arr1 = { 10, 20, 30, 40, 50 };
-            MaxNum<int>.Max(arr1);
+            int max1 = MaxNum<int>.Max(arr1);
+            Console.WriteLine("Maximum Value for Int is: " + max1);
 
             Console.WriteLine();
             double[] arr2 = { 10.5, 20.5, 30.5, 40.5, 50.5 };
-             MaxNum<double>.Max(arr2);
+            double max2 = MaxNum<double>.Max(arr2);
+            Console.WriteLine("Maximum Value for Float is: " + max2);
 
             Console.WriteLine();
             string[] arr3 = { "My", "name", "is", "Rachit", "Rathi" };
-            MaxNum<string>.Max(arr3);
+            string max3 = MaxNum<string>.Max(arr3);
+            Console.WriteLine("Maximum Value for String is: " + max3);
         }
     }
 }
