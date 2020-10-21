2eb91bcf4f27a6c17de90f6635c067c91e2e8fbb
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-21 20:10:10 +0530

    [Rachit] Add. Maximum Problem Generics

---
+++ MaxNumTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaxNumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
